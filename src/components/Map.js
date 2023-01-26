cf7478328d1a502eea97546f253a05514b81ea2e
Anthony Lopez <Antl21292@gmail.com>
2023-01-26 18:58:45 -0500

    image added

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+
 class Map extends React.Component {
   render() {
-    return (<p>Map Test</p>)
+    return (
+      <img src={this.props.mapImage} alt=""/>
+    )
   }
 }
 
