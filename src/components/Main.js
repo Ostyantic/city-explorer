1e328724a52036e8a2b169a8f937ea976cda1903
Anthony Lopez <Antl21292@gmail.com>
2023-01-26 00:01:29 -0500

    Added a form and passed two components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React from "react";
+import Map from "./Map";
+import Restaurant from "./Restaurant";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class Main extends React.Component {
   render() {
-    return (<p>Hello World!</p>)
+    return (
+      <>
+      <Container>
+        <Form>
+          <Form.Group>
+            <Form.Label>City</Form.Label>
+            <Form.Control type="text" placeholder="City"/>
+            <Button variant="primary" type="submit" onClick={this.displaySearch}>
+              Search
+            </Button>
+          </Form.Group>
+        </Form>
+      </Container>
+
+      <Map />
+      <Restaurant />
+      </>
+    );
   }
 }
 
