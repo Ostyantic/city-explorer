40f0aeaf2dff76691cb1e798c48a89e4ac7809c9
Anthony Lopez <Antl21292@gmail.com>
2023-01-26 14:10:54 -0500

    cleaning up readability

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,65 +7,69 @@ import Button from "react-bootstrap/Button";
 import axios from "axios";
 
 class Main extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       displayInfo: false,
-      city: '',
-      cityData: ''
-    }
+      city: "",
+      cityData: "",
+    };
   }
 
-  handleSearchInput = e => {
+  handleSearchInput = (e) => {
     let cityName = e.target.value;
-    this.setState({
-      city: cityName
-    },
-    () => console.log(this.state.city)
+    this.setState(
+      {
+        city: cityName,
+      },
+      () => console.log(this.state.city)
     );
-  }
+  };
 
-  handleDisplaySearch = async e => {
+  handleDisplaySearch = async (e) => {
     e.preventDefault();
 
-    let response = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`);
+    let response = await axios.get(
+      `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`
+    );
 
     console.log(response.data[0]);
 
     this.setState({
       displayInfo: true,
-      cityData: response.data[0]
+      cityData: response.data[0],
     });
-    
-  }
+  };
 
   render() {
     return (
       <>
-      <Container>
-        <Form>
-          <Form.Group>
-            <Form.Label>City</Form.Label>
-            <Form.Control 
-            type="text" 
-            placeholder="City"
-            onInput={this.handleSearchInput}/>
-            <Button 
-            onClick={this.handleDisplaySearch}>Explore!</Button>
-          </Form.Group>
-        </Form>
-      </Container>
+        <Container>
+          <Form>
+            <Form.Group>
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="City"
+                onInput={this.handleSearchInput}
+              />
+              <Button onClick={this.handleDisplaySearch}>Explore!</Button>
+            </Form.Group>
+          </Form>
+        </Container>
 
-      {this.state.displayInfo &&
-      <>
-        <Location 
-        cityName={this.state.cityData.display_name}
-        cityLat={this.state.cityData.lat}
-        cityLon={this.state.cityData.lon}/>
-      </>
-      }
+        {this.state.displayInfo && (
+          <>
+            <Container>
+              <Location
+                cityName={this.state.cityData.display_name}
+                cityLat={this.state.cityData.lat}
+                cityLon={this.state.cityData.lon}
+              />
+            </Container>
+          </>
+        )}
       </>
     );
   }
