1cd938f3fa0f782d1fdb90055d84cfebdb50765b
Anthony Lopez <Antl21292@gmail.com>
2023-01-26 18:56:44 -0500

    small change

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return (<h1>City Explorer!</h1>)
+    return (
+      <header>
+        <h1>City Explorer!</h1>
+      </header>
+    )
   }
 }
 
