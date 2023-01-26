43f14311488e11cfd0e357be59af0bab13a41cc4
Anthony Lopez <Antl21292@gmail.com>
2023-01-26 18:57:21 -0500

    added card image

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+// import Image from 'react-bootstrap/Image'
 
 class Location extends React.Component {
   render() {
     return (
       <Card bg='dark'>
-        <Card.Header as={'h2'}>City Info: {this.props.cityName}</Card.Header>
+        <Card.Header as={'h2'}>{this.props.cityName}</Card.Header>
         <Card.Body>Lat: {this.props.cityLat}, Long: {this.props.cityLon}</Card.Body>
+        <Card.Img src={this.props.mapImage}/>
       </Card>
     )
   }
