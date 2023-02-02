a95f19b602d8181ac00f4470196410913ad2bd1f
Anthony Lopez <Antl21292@gmail.com>
2023-02-01 21:08:05 -0500

    small change to state

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,8 +18,9 @@ class Main extends React.Component {
       cityData: "",
       locationLat: "",
       locationLon: "",
-      date: "",
-      forecast: "",
+      weather: "",
+      // date: "",
+      // forecast: "",
     };
   }
 
@@ -35,94 +36,92 @@ class Main extends React.Component {
   };
 
   handleShowError = (e) => {
-
     this.setState({
       showError: false,
-    })
-
-  }
+    });
+  };
 
   handleDisplaySearch = async (e) => {
     e.preventDefault();
 
-    try{
-        let locationResponse = await axios.get(
-          `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`
-        );
-    
-        let serverResponse = await axios.get(`http://localhost:3001/weather?searchQuery=${this.state.city}`)
-    
-        // console.log(locationResponse.data[0]);
-        // console.log(locationResponse.data[0].lat);
-        // console.log(locationResponse.data[0].lon);
-        console.log(serverResponse);
-        
-    
-    
-        if(!this.state.showError){
-          this.setState(
-          {
-            displayInfo: true,
-            cityData: locationResponse.data[0],
-            locationLat: locationResponse.data[0].lat,
-            locationLon: locationResponse.data[0].lon,
-            date: serverResponse.data[0].date,
-            forecast:serverResponse.data[0].desc,
-          }
-          );
-        }
+    try {
+      let locationResponse = await axios.get(
+        `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`
+      );
 
-    }
-    catch(error){
+      let serverResponse = await axios.get(
+        `${process.env.REACT_APP_SERVER}/weather?searchQuery=${this.state.city}`
+      );
+
+      // console.log(locationResponse.data[0]);
+      // console.log(locationResponse.data[0].lat);
+      // console.log(locationResponse.data[0].lon);
+      console.log(serverResponse);
+
+      if (!this.state.showError) {
+        this.setState({
+          displayInfo: true,
+          cityData: locationResponse.data[0],
+          locationLat: locationResponse.data[0].lat,
+          locationLon: locationResponse.data[0].lon,
+          weather: serverResponse.data,
+          // date: serverResponse.data.date,
+          // forecast:serverResponse.data.desc,
+        });
+      }
+    } catch (error) {
       console.log(error);
-      this.setState(
-        {
-          showError: true,
-        }
-      )
+      this.setState({
+        showError: true,
+      });
     }
     // console.log(this.state.cityMap);
-
   };
 
   render() {
     return (
       <>
-        <Container className="formContainer">
-          <Form className="d-flex justify-content-center" onSubmit={this.handleDisplaySearch}>
-            <Form.Group>
-              <Form.Label style={{textAlign:'center'}}>City</Form.Label>
-              <Form.Group className="d-flex">
-                <Form.Control
-                  type="text"
-                  placeholder="City"
-                  onChange={this.handleSearchInput}
-                  style={{width:'200px'}}
-                />
-                <Button onClick={this.handleDisplaySearch}>Explore!</Button>
+        <Container className="parentContainer">
+          
+          <Container className="formContainer">
+            <Form
+              className="d-flex justify-content-center"
+              onSubmit={this.handleDisplaySearch}
+            >
+              <Form.Group>
+                <Form.Label style={{ textAlign: "center" }}>City</Form.Label>
+                <Form.Group className="d-flex">
+                  <Form.Control
+                    type="text"
+                    placeholder="City"
+                    onChange={this.handleSearchInput}
+                    style={{ width: "200px" }}
+                  />
+                  <Button onClick={this.handleDisplaySearch}>Explore!</Button>
+                </Form.Group>
               </Form.Group>
-            </Form.Group>
-          </Form>
+            </Form>
+          </Container>
+
+          {this.state.displayInfo && (
+            <Container className="location">
+              <Location
+                mapImage={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationLat},${this.state.locationLon}&zoom=12&markers=icon:small-red-cutout|${this.state.locationLat},${this.state.locationLon}`}
+                cityName={this.state.cityData.display_name}
+                cityLat={this.state.cityData.lat}
+                cityLon={this.state.cityData.lon}
+              />
+              <Weather
+                className="weather"
+                weather={this.state.weather}
+                // date={this.state.date}
+                // forecast={this.state.forecast}
+              />
+            </Container>
+          )}
         </Container>
 
-        {this.state.displayInfo && (
-          <Container className="location">
-            <Location
-              mapImage={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationLat},${this.state.locationLon}&zoom=12&markers=icon:small-red-cutout|${this.state.locationLat},${this.state.locationLon}`}
-              cityName={this.state.cityData.display_name}
-              cityLat={this.state.cityData.lat}
-              cityLon={this.state.cityData.lon}
-            />
-            <Weather className="weather"
-              date={this.state.date}
-              forecast={this.state.forecast}
-            />
-          </Container>
-        )}
-
-        {this.state.showError && (
-          <Error show={this.handleShowError}/>
-        )}
+        {this.state.showError && <Error show={this.handleShowError} />}
       </>
     );
   }
