4eae6ef45aa1c4c7e779e7c9368ff6be723fca16
Anthony Lopez <Antl21292@gmail.com>
2023-02-04 21:32:28 -0500

    refactored

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,23 +1,25 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import Movie from "./Movie";
 
-class Movies extends React.Component{
+class Movies extends React.Component {
   render() {
-    return(
-    <>
-      <h2 className="moviesHeader">Movies</h2>
-      <div className="movieCards">
-      {this.props.movies.map((element, idx) => {
-        return(
-          <Card bg="dark" key={idx} style={{width:"14rem"}}>
-            <Card.Header>{element.title}</Card.Header>
-            <Card.Img src={`https://www.themoviedb.org/t/p/w500/${element.img_url}`} alt={element.title} height={300} width={200}/>
-            <Card.Body>{element.overview}</Card.Body>
-          </Card>
-        )
-      })}
-      </div>
-    </>)
+    return (
+      <>
+        <h2 className="moviesHeader">Movies</h2>
+        <div className="movieCards">
+          {this.props.movies.map((element, idx) => {
+            return (
+              <Movie
+                key={idx}
+                movieTitle={element.title}
+                movieImg={`https://www.themoviedb.org/t/p/w500/${element.img_url}`}
+                movieDesc={element.overview}
+              />
+            );
+          })}
+        </div>
+      </>
+    );
   }
 }
 
