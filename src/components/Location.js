69646758358fdee953981adaacbd7289171b30c1
Anthony Lopez <Antl21292@gmail.com>
2023-02-04 19:50:33 -0500

    Added some card styling

--- src/components/Location.js
+++ src/components/Location.js
@@ -5,7 +5,7 @@ import { Card } from 'react-bootstrap';
 class Location extends React.Component {
   render() {
     return (
-      <Card bg='dark'>
+      <Card bg='dark' style={{width:"450px", height:"auto", margin:"auto"}}>
         <Card.Header as={'h2'}>{this.props.cityName}</Card.Header>
         <Card.Img src={this.props.mapImage}/>
         <Card.Body>Lat: {this.props.cityLat}, Long: {this.props.cityLon}</Card.Body>
