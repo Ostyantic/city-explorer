26431d56df9fbfb45f24b82d6caad092309baeae
Anthony Lopez <Antl21292@gmail.com>
2023-02-04 21:32:47 -0500

    newly created and appropriate props passed

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
-class Movie extends React.Component{
+class Movie extends React.Component {
   render() {
-    return(<></>)
+    return (
+      <>
+        <Card bg="dark" style={{ width: "14rem" }}>
+          <Card.Header>{this.props.movieTitle}</Card.Header>
+          <Card.Img
+            src={this.props.movieImg}
+            alt={this.props.movieTitle}
+            height={300}
+            width={200}
+          />
+          <Card.Body>{this.props.movieDesc}</Card.Body>
+        </Card>
+      </>
+    );
   }
 }
 
