2589af3f8c7b8a5b6edd66a155f8c8ee0ba71a29
Anthony Lopez <Antl21292@gmail.com>
2023-02-04 19:52:54 -0500

    added some classNames and styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,13 +4,15 @@ class Weather extends React.Component{
   render() {
     return(
     <>
-      <h2>Daily Forecast</h2>
-      <ul>
+      <h2 className="weatherHeader">Daily Forecast</h2>
+      <ul className="weatherList">
       {this.props.weather.map((element, idx) => {
         return(
-          <li key={idx}>
-            <p>Date: {element.date}</p>
-            <p>Forecast: {element.desc}</p>   
+          <li className="weatherDay" style={{textAlign:"center"}} key={idx}>
+            <h5 style={{background:"black"}}>Date</h5>
+            <p>{`${element.date}`}</p>
+            <h5 style={{background:"black"}}>Forecast</h5>   
+            <p>{`${element.desc}`}</p>
           </li>
         )
       })}
