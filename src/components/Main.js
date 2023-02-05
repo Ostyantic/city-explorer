fc8ce25c37aab21fd04b11bd1faf244d1790ef4c
Anthony Lopez <Antl21292@gmail.com>
2023-02-04 20:53:34 -0500

    refactored and removed comments

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,8 @@ import Error from "./ErrorMsg";
 import Location from "./Location";
 import Weather from "./Weather";
 import Movies from "./Movies";
+import Search from "./Search";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 
@@ -21,8 +21,6 @@ class Main extends React.Component {
       locationLon: "",
       weather: [],
       movies:[],
-      // date: "",
-      // forecast: "",
     };
   }
 
@@ -37,7 +35,7 @@ class Main extends React.Component {
     );
   };
 
-  handleShowError = (e) => {
+  handleShowError = () => {
     this.setState({
       showError: false,
     });
@@ -50,21 +48,12 @@ class Main extends React.Component {
       let locationResponse = await axios.get(
         `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`
       );
-
-      // console.log(locationResponse.data);
-      // console.log(locationResponse.data[0].lat);
-      // console.log(locationResponse.data[0].lon);
-    
-
       if (!this.state.showError) {
         this.setState({
           displayInfo: true,
           cityData: locationResponse.data[0],
           locationLat: locationResponse.data[0].lat,
           locationLon: locationResponse.data[0].lon,
-
-          // date: serverResponse.data.date,
-          // forecast:serverResponse.data.desc,
         });
       }
     } catch (error) {
@@ -73,22 +62,16 @@ class Main extends React.Component {
         showError: true,
       });
     }
-    // console.log(this.state.cityMap);
   };
 
   handleDisplayWeather = async () => {
-
-
     try{
-
       let serverResponse = await axios.get(
         `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.locationLat}&lon=${this.state.locationLon}`
       );
-
       this.setState({
         weather: serverResponse.data,
       })
-
       console.log(serverResponse);
     }
     catch(error) {
@@ -97,7 +80,6 @@ class Main extends React.Component {
   }
 
   handleDisplayMovies = async () => {
-
     try {
       let movieResponse = await axios.get(
         `${process.env.REACT_APP_SERVER}/movie?city=${this.state.city}`
@@ -116,25 +98,11 @@ class Main extends React.Component {
     return (
       <>          
           <Container className="formContainer">
-            <Form
-              className="d-flex justify-content-center"
-              onSubmit={this.handleDisplaySearch}
-            >
-              <Form.Group>
-                <Form.Label style={{ textAlign: "center" }}>City</Form.Label>
-                <Form.Group className="d-flex">
-                  <Form.Control
-                    type="text"
-                    placeholder="City"
-                    onChange={this.handleSearchInput}
-                    style={{ width: "200px" }}
-                  />
-                  <Button onClick={this.handleDisplaySearch}>Explore!</Button>
-                </Form.Group>
-              </Form.Group>
-            </Form>
+            <Search 
+              changeInput={this.handleSearchInput}
+              submit={this.handleDisplaySearch}
+            />
           </Container>
-
           {this.state.displayInfo && (
             <Container className="location">
               <Location
@@ -153,14 +121,12 @@ class Main extends React.Component {
               <Weather
                 className="weather"
                 weather={this.state.weather}
-                // forecast={this.state.forecast}
               />}
               {this.state.movies.length > 0 &&
               <Movies 
                 className="movies"
                 movies={this.state.movies}
               />}
-            {/* </Container> */}
 
         {this.state.showError && <Error show={this.handleShowError} />}
       </>
